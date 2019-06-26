390dc7458b136faf2e4e4db4c2232e20625fc5b8
SteveNelson83 <51002138+SteveNelson83@users.noreply.github.com>
2019-06-26 04:09:52 +0100

    added adopt and have baby functions

--- __tests__/pet.tests.js
+++ __tests__/pet.tests.js
@@ -9,23 +9,22 @@ describe('constructor', () => {
     expect(pet.name).toEqual('Fido');
     })
     it('has a initial age of 0', () => {
-        const pet = new Pet('Fido');
+        const pet = new Pet();
         expect(pet.age).toEqual(0);
     })
     it('pet gains hunger and loses fitness with age', () => {
-        const pet = new Pet('Bobby');
+        const pet = new Pet();
         pet.growUp();
         expect(pet.hunger).toEqual(5);
         expect(pet.fitness).toEqual(7);
     })
-    it('pet aged 2 gains fitness after a walk', () => {
+    it('pet gains fitness after a walk', () => {
         const pet = new Pet();
-        pet.growUp();
-        pet.growUp();
+        pet.fitness = 4;
         pet.walk();
         expect(pet.fitness).toEqual(8);
     })
-    it('returns 10 for fitness after a walk when fitness was originally greater than 6', () => {
+    it('walk function does not return fitness higher than 10', () => {
         const pet = new Pet();
         pet.growUp();
         pet.walk();
@@ -46,11 +45,7 @@ describe('constructor', () => {
     })
     it('checkup method returns I need a walk if fitness is 3 or lower', () => {
         const pet = new Pet();
-        pet.growUp();
-        pet.growUp();
-        pet.feed();
-        pet.growUp();
-        pet.feed();
+        pet.fitness = 3;
         expect(pet.checkup()).toEqual('I need a walk');
     })
     it('checkup method returns I feel great! if fit and not hungry', () => {
@@ -61,4 +56,26 @@ describe('constructor', () => {
         const pet = new Pet();
         expect(pet.isAlive).toBe(true);
     })
+    it('checkup function returns Your pet is no longer alive if pet is dead', () => {
+        const pet = new Pet();
+        pet.age = 30;
+        expect(pet.checkup()).toEqual('Your pet is no longer alive :(');
+    })
+    it('growUp and walk functions throw errors if pet is dead', () => {
+        const pet = new Pet();
+        pet.age = 30;
+        expect(() => pet.growUp()).toThrow('Your pet is no longer alive :(');
+        expect(() => pet.walk()).toThrow('Your pet is no longer alive :(');
+    })
+    it('adoptChild function adds child to children property array', () => {
+        const parent = new Pet();
+        const child = new Pet('Amelia');
+        parent.adoptChild(child);
+        expect(parent.children).toContain(child);
+    })
+    it('haveBaby function adds baby to children property array', () => {
+        const pet = new Pet ();
+        pet.haveBaby('Dave');
+        expect(pet.children).toBeInstanceOf(Object);
+    } )
 });
--- src/pet.js
+++ src/pet.js
@@ -4,6 +4,7 @@ function Pet (name) {
     this.age = 0;
     this.hunger = 0;
     this.fitness = 10;
+    this.children = [];
 }
 
 Pet.prototype = {
@@ -11,21 +12,41 @@ Pet.prototype = {
         return this.age < 30 && this.hunger < 10 && this.fitness > 0;
     },
     growUp: function () {
+        if (!this.isAlive) {
+            throw new Error('Your pet is no longer alive :(');
+          }
         this.age += 1;
         this.hunger += 5;
         this.fitness -= 3;
     },
     walk: function () {
-        this.fitness > 6 ? this.fitness = 10 : this.fitness += 4;
+        if (!this.isAlive) {
+            throw new Error('Your pet is no longer alive :(');
+          }
+        if (this.fitness > 6) {
+            return this.fitness = 10 
+        }
+        return this.fitness += 4;
     },
     feed: function () {
+        if (!this.isAlive) {
+            throw new Error('Your pet is no longer alive :(');
+          }
         this.hunger < 3 ? this.hunger = 0 : this.hunger -= 3
     },
     checkup: function () {
         return (this.fitness <= 3) ? 'I need a walk' 
         : (this.hunger >= 5) ? 'I am hungry' 
         : ((this.fitness <= 3) && (this.hunger >= 5)) ? 'I am hungry AND I need a walk' 
+        : this.isAlive === false ? 'Your pet is no longer alive :('
         : 'I feel great!';
+    },
+    adoptChild: function (child) {
+        this.children.push(child);
+    },
+    haveBaby: function (name) {
+        const baby = new Pet (name);
+        this.children.push(baby);
     }
 };
 
